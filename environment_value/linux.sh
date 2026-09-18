2352e5e518f5656dfb1c97949550487622c8a21d
apec9 <apec911@foxmail.com>
2026-09-18 13:44:32 +0800

    🔒更新项目部署配置文件

--- environment_value/linux.sh
+++ environment_value/linux.sh
@@ -1,35 +1,67 @@
 #!/usr/bin/env bash
+# 建议保存为：/etc/profile.d/app-env.sh
+# 生效：source /etc/profile.d/app-env.sh  或重新登录
 
 # nacos配置
-export NACOS_SERVER_HOST_PORT="192.168.99.128:8848"
+export NACOS_SERVER_HOST_PORT=""
 export NACOS_GROUP="DOTM_GROUP"
+export VI_GROUP="VI_GROUP"
 
 # 消息队列配置
-export RABBITMQ_SERVER_HOST="192.168.99.128"
-export RABBITMQ_SERVER_PORT="5672"
-export RABBITMQ_SERVER_USERNAME="rabbitmq"
-export RABBITMQ_SERVER_PASSWORD="rabbitmq"
+export RABBITMQ_SERVER_HOST=""
+export RABBITMQ_SERVER_PORT=""
+export RABBITMQ_SERVER_USERNAME=""
+export RABBITMQ_SERVER_PASSWORD=""
 
 # 数据库配置
-export DATASOURCE_HOST_PORT="192.168.99.128:3306"
-export DATASOURCE_DATABASES="vi"
-export DATASOURCE_USERNAME="root"
-export DATASOURCE_PASSWORD="mysql_AWHzE5"
+export DATASOURCE_HOST_PORT=""
+export DATASOURCE_DATABASES=""
+export DATASOURCE_USERNAME=""
+export DATASOURCE_PASSWORD=""
 
 # redis配置
-export REDIS_HOST="192.168.99.128"
-export REDIS_PORT="6379"
-export REDIS_PASSWORD="redis_Btc43t"
+export REDIS_HOST=""
+export REDIS_PORT=""
+export REDIS_PASSWORD=""
 
 # 网关配置
-export GATEWAY_PROFILES="gateway,common"
-
+export GATEWAY_PROFILES="gateway,common,auth"
 # 认证中心配置
-export AUTH_PROFILES="auth,dev,common"
+export AUTH_PROFILES="auth,dev,common,ai"
+# 认证监控配置
+export AUTH_MONITOR_PROFILE="common,dev"
+# 系统监控配置
+export SYSTEM_MONITOR_PROFILE="common,dev"
+# 文件上传模块配置
+export FILE_PROFILE="dev,file"
+
+# VI文件模块
+export SERVICE_FILE_PROFILE="dev,file,common"
 
 # 微服务端口配置
-export GATEWAY_PORT="8080"          # 网关
-export AUTH_PORT="1000"             # 认证中心
-export AUTH_MONITOR_PORT="9999"     # 认证中心监控
-export SYSTEM_MONITOR_PORT="9998"   # 系统监控
-export FILE_PORT="9997"             # 文件
+export GATEWAY_PORT="8080"        # 网关
+export AUTH_PORT="1000"           # 认证中心
+export AUTH_MONITOR_PORT="9999"   # 认证中心监控
+export SYSTEM_MONITOR_PORT="9998" # 系统监控
+export FILE_PORT="9997"           # 文件
+
+# 业务服务端口配置
+export SERVICE_FILE="1001"        # VI文件服务
+
+# jwt密钥配置
+export SECRET_KEY=""
+
+# github认证密钥配置
+export CLIENT_ID=""
+export CLIENT_SECRET=""
+
+# AI的密钥配置
+export API_KEY=""
+
+# 阿里云OSS密钥配置
+export OSS_ACCESS_KEY_ID=""
+export OSS_ACCESS_KEY_SECRET=""
+
+# MinIO密钥配置
+export MINIO_ACCESS_KEY=""
+export MINIO_SECRET_KEY=""
