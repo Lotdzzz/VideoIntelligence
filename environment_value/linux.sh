c5060d16b01c49ba9b8c934d30dfdb483a3c76db
apec9 <apec911@foxmail.com>
2026-09-22 21:38:28 +0800

    🔧针对python开发的配置更新

--- environment_value/linux.sh
+++ environment_value/linux.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/env bash
-# 建议保存为：/etc/profile.d/app-env.sh
-# 生效：source /etc/profile.d/app-env.sh  或重新登录
+#!/bin/bash
+# 原 Windows 注册表环境变量转 Linux Shell 环境变量
+# 建议保存为 /etc/profile.d/app-env.sh
+# 生效：source /etc/profile.d/app-env.sh
 
 # nacos配置
 export NACOS_SERVER_HOST_PORT=""
+export NACOS_SERVER_IP=""
 export NACOS_GROUP="DOTM_GROUP"
+export NACOS_DEFAULT_GROUP="DEFAULT_GROUP"
+export NACOS_NAMESPACE="public"
 export VI_GROUP="VI_GROUP"
 
 # 消息队列配置
@@ -39,14 +43,14 @@ export FILE_PROFILE="dev,file"
 export SERVICE_FILE_PROFILE="dev,file,common"
 
 # 微服务端口配置
-export GATEWAY_PORT="8080"        # 网关
-export AUTH_PORT="1000"           # 认证中心
-export AUTH_MONITOR_PORT="9999"   # 认证中心监控
-export SYSTEM_MONITOR_PORT="9998" # 系统监控
-export FILE_PORT="9997"           # 文件
-
+export GATEWAY_PORT="8080"          # 网关
+export AUTH_PORT="1000"             # 认证中心
+export AUTH_MONITOR_PORT="9999"     # 认证中心监控
+export SYSTEM_MONITOR_PORT="9998"   # 系统监控
+export FILE_PORT="9997"             # 文件
 # 业务服务端口配置
-export SERVICE_FILE="1001"        # VI文件服务
+export SERVICE_FILE="1001"          # VI文件服务
+export SERVICE_FILE_PYTHON="1002"   # python端文件服务
 
 # jwt密钥配置
 export SECRET_KEY=""
